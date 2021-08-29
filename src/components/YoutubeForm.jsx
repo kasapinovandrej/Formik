03926696a671021fed1fe790a86e2c4099ff0d1d
Andrej Kasapinov <72466548+kasapinovandrej@users.noreply.github.com>
2021-08-29 18:49:15 +0200

    formik tutorial

---
+++ src/components/YoutubeForm.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import * as Yup from "yup";
+import TextError from "./TextError";
+
+const initialValues = {
+  name: "",
+  email: "",
+  channel: "",
+  comments: "",
+  address: "",
+  social: {
+    facebook: "",
+    twitter: "",
+  },
+  phoneNumbers: ["", ""],
+  phNumbers: [""],
+};
+const onSubmit = (values) => {
+  console.log(values);
+};
+
+const validationSchema = Yup.object({
+  name: Yup.string().required("Required!"),
+  email: Yup.string().email("Invalid email format").required("Required!"),
+  channel: Yup.string().required("Required!"),
+  comments: Yup.string().required("Required!"),
+  address: Yup.string().required("Required!"),
+});
+
+const YoutubeForm = () => {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className="form-control">
+          <label htmlFor="name">Name</label>
+          <Field type="text" id="name" name="name" />
+          <ErrorMessage name="name" component={TextError} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="email">E-mail</label>
+          <Field type="email" id="email" name="email" />
+          <ErrorMessage name="email">
+            {(errorMsg) => {
+              return <div className="error">{errorMsg}</div>;
+            }}
+          </ErrorMessage>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="channel">Channel</label>
+          <Field type="text" id="channel" name="channel" />
+          <ErrorMessage name="channel" component={TextError} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="comments">Comments</label>
+          <Field as="textarea" id="comments" name="comments" />
+          <ErrorMessage name="comments" component={TextError} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="address">Address</label>
+          <Field name="address">
+            {({ field, form, meta }) => (
+              <div>
+                <input id="address" type="text" {...field} />
+                {meta.touched && meta.error && <div>{meta.error}</div>}
+              </div>
+            )}
+          </Field>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">Facebook</label>
+          <Field type="text" id="facebook" name="social.facebook" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <Field type="text" id="twitter" name="social.twitter" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primary">Primary phone number</label>
+          <Field type="text" id="primary" name="phoneNumbers[0]" />
+        </div>
+        <div className="form-control">
+          <label htmlFor="secondary">Secondary phone number</label>
+          <Field type="text" id="secondary" name="phoneNumbers[1]" />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="phNumbers">List of phone numbers</label>
+          <FieldArray name="phNumbers" id="phNumbers">
+            {({
+              push,
+              remove,
+              form: {
+                values: { phNumbers },
+              },
+            }) => {
+              console.log(phNumbers);
+              return (
+                <div>
+                  {phNumbers.map((phone, index) => (
+                    <div key={index}>
+                      <Field name={`phNumbers[${index}]`} />
+                      {index > 0 && (
+                        <button type="button" onClick={() => remove(index)}>
+                          -
+                        </button>
+                      )}
+
+                      <button type="button" onClick={() => push("")}>
+                        +
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            }}
+          </FieldArray>
+        </div>
+
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default YoutubeForm;
